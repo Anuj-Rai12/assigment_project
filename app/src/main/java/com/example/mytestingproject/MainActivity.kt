1075bf2a1acdbf50237398fc37e0b74ce35b2b12
Anuj <anujrai04082002.0@gmail.com>
2022-10-25 00:24:36 +0530

    Working on Auth status:Success

--- app/src/main/java/com/example/mytestingproject/MainActivity.kt
+++ app/src/main/java/com/example/mytestingproject/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mytestingproject.databinding.ActivityMainBinding
+import com.zoho.desk.asap.api.ZDPortalCallback
+import com.zoho.desk.asap.api.ZDPortalException
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,8 +16,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val flag=MainApplication.apiProvider?.isUserSignedIn
+        flag?.let {
+            if (!it){
+                doLogin()
+            }else{
+                toastMsg("user is already signed In")
+            }
+        }?: run {
+            toastMsg("user signed is null")
+        }
         binding.goTo.setOnClickListener {
             startActivity(Intent(this,MyZohoActivity::class.java))
         }
+    }
+
+    private fun doLogin() {
+        MainApplication.apiProvider?.setUserToken("Anujraiak@outlook.com",object :ZDPortalCallback.SetUserCallback{
+            override fun onException(exp: ZDPortalException?) {
+                toastMsg("Error")
+                setLogCat("SubmitTicket",exp?.errorMsg?:"Unknown Error")
+            }
+
+            override fun onUserSetSuccess() {
+                toastMsg("user is Logged In")
+            }
+
+        })
     }
 }
