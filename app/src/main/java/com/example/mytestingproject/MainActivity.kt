509c7902f4734ba5c969ed8bc4f23a8bd985fa5e
Anuj-Rai12 <anujrai04082002.0@gmail.com>
2023-01-14 00:04:42 +0530

    Working on On Camera

--- app/src/main/java/com/example/mytestingproject/MainActivity.kt
+++ app/src/main/java/com/example/mytestingproject/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.mytestingproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mytestingproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnId.setOnClickListener {
+
+        }
+
     }
 }
