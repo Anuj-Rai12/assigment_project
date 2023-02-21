0da14696da2b3be31f4aac3012937e765b1631b3
Anuj-Rai12 <anujrai04082002.0@gmail.com>
2023-02-21 18:46:35 +0530

    Working on the Pax Store status:Successfully

--- app/src/main/java/com/example/mytestingproject/MainActivity.kt
+++ app/src/main/java/com/example/mytestingproject/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.mytestingproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mytestingproject.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.paxStore.setOnClickListener {
+            val launchIntent =
+                packageManager.getLaunchIntentForPackage("com.pax.market.android.app")
+
+            if (launchIntent != null) {
+                startActivity(launchIntent)
+            } else {
+                Toast.makeText(
+                    this@MainActivity,
+                    "There is no package available in android",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
+        }
+
     }
 }
